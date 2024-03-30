547a12c3d0e018f6be1a6b54e24a32d78862f1f3
maycondbraga <maycondbraga.s@gmail.com>
2024-03-30 01:34:06 -0300

    feat: improving swagger with authentication

--- src/main/java/com/bmaycon/schedule/config/SwaggerConfig.java
+++ src/main/java/com/bmaycon/schedule/config/SwaggerConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +24,28 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.bmaycon.schedule.api"))
                 .paths(PathSelectors.any())
                 .build()
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .apiInfo(apiInfo());
+    }
+
+    public ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    public SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(securityReferences())
+                .build();
+    }
+
+    public List<SecurityReference> securityReferences() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+
+        return List.of(new SecurityReference("JWT", authorizationScopes));
     }
 
     public ApiInfo apiInfo() {
